335dfc63a9d72996d4da782ce11fde7d0f92cc73
franqitork <franco.a.maio@gmail.com>
2025-03-21 15:54:16 -0300

    cambie el lenguaje a c pero no lo pude probar

---
+++ ejercicio 1/MI_1.c
@@ -0,0 +1,9 @@
+int main(){
+    int ia, ib,rta;
+    printf("Este programa suma ambas entradas y entrega el resultado");
+    scanf("Ingrese un número entero:\n---> %d",&ia);
+    scanf("Ingrese otro número entero:\n---> %d",&ib);
+    rta=ia+ib;
+    printf("El resultado es %d",rta);
+    return 0;
+}
