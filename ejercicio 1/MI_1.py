fff8c369805d00db045ed057d4e820c70d028dc1
franqitork <franco.a.maio@gmail.com>
2025-03-21 12:50:19 -0300

    Agrego ejercicio 1

---
+++ ejercicio 1/MI_1.py
@@ -0,0 +1,5 @@
+print("Este programa suma ambas entradas y entrega el resultado")
+ia=int(input("Ingrese un número entero:\n---> "))
+ib=int(input("Ingrese otro número entero:\n---> "))
+rta=ia+ib
+print("el resultado es",rta)
